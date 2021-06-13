b33ae454fd82845d839e52492ec964a0868d3a88
Vector <81518724+Vector717@users.noreply.github.com>
2021-06-14 00:28:35 +0530

    Add files via upload

---
+++ Day 7/object.js
@@ -0,0 +1,54 @@
+// Question 1
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 }
+// console.log(Object.keys(student))
+
+// Question 2
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 }
+// console.log(student);
+// delete student.rollno;
+// console.log(student);
+
+// Question 3
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 }
+// console.log(Object.keys(student).length);
+
+// Question 4
+ var library = [ 
+    {title: 'Bill Gates',author: 'The Road Ahead',readingStatus: true},
+    {title: 'Steve Jobs',author: 'Walter Isaacson',readingStatus: true},
+    {title: 'Mockingjay: The Final Book of The Hunger Games',author: 'Suzanne Collins',
+    readingStatus: false
+}];
+// for (var i = 0; i < library.length; i++) 
+// {
+//   var book = "'" + library[i].title + "'" + ' by ' + library[i].author + ".";
+// if (library[i].readingStatus) {
+//   console.log("Read - " + book);
+// } else
+// {
+//  console.log("Not Read - " + book);
+// }
+//}
+ 
+// Question 5
+function volume(r,h){
+    cylinder = (3.14*r**2*h)
+    return cylinder.toFixed(4);
+}
+// console.log(volume(4,6))
+
+// Question 6
+
+var library2 = [{title: "The Road Ahead", author: "Bill Gates",libraryID: 1254,},
+{title: "Walter Isaacson",author: "Steve Jobs",libraryID: 4264,},
+{title: "Mockingjay: The Final Book of The Hunger Games",author: "Suzanne Collins",
+libraryID: 3245,},];
+//library2.sort((a, b) => b.libraryID - a.libraryID);
+//console.log(library2);
+
+
+
+
+ 
+    
+
