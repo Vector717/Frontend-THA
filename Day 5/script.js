019fb9f3b57e07f251716389d6d98570a3d0b128
Vector <81518724+Vector717@users.noreply.github.com>
2021-07-19 20:16:49 +0530

    day 5 completed

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const firstInput = document.getElementById('one');
 const secondInput = document.getElementById('two');
 const controls = document.querySelector('.controls');
@@ -40,47 +39,4 @@ function controlHandler(e) {
 function helper(res) {
 	console.log(res);
 	resultBox.innerText = res;
-=======
-const firstInput = document.getElementById('one');
-const secondInput = document.getElementById('two');
-const controls = document.querySelector('.controls');
-const resultBox = document.getElementById('result-box');
-
-controls.addEventListener('click', controlHandler);
-
-function controlHandler(e) {
-	const valOne = Number(firstInput.value);
-	const valTwo = Number(secondInput.value);
-
-	// console.log(valOne, valTwo);
-
-	if (!valOne || !valTwo) {
-		resultBox.innerText = 'Provide a valid input';
-		return;
-	}
-
-	const cmd = e.target.innerText;
-	// console.log(cmd);
-	switch (cmd) {
-		case 'Add':
-			helper(valOne + valTwo);
-			break;
-		case 'Subtract':
-			helper(valOne - valTwo);
-			break;
-		case 'Multiply':
-			helper(valOne * valTwo);
-			break;
-		case 'Division':
-			helper(valOne / valTwo);
-			break;
-		default:
-			console.log('default');
-	}
-}
-
-function helper(res) {
-	console.log(res);
-	resultBox.innerText = res;
->>>>>>> 0ee797794569f70e619b499ac3a2e778f3a0cbf3
 }
